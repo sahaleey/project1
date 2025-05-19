8e2c35107eb92df299ef3a8b1bde708629cd7eca
crazydrace <ajua46244@gmail.com>
2025-05-19 10:02:27 +0530

    allowed one more orgin

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 # ----------------------- CORS -----------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
